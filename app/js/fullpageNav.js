b41b34e30fef57de003ed21e953949f9ef7e969c
amitil <biloholovskyi>
2020-04-22 09:24:45 +0300

    fix fullPage

--- app/js/fullpageNav.js
+++ app/js/fullpageNav.js
@@ -1,17 +1,27 @@
 const sections = $('.fullPage');
 
 const fullPageNav = (e) => {
-  sections.css('transform', `translateY(-${+e.target.getAttribute('data-number') - 1}00vh)`);
+  const index = +e.target.getAttribute('data-number') - 1;
+  console.log(index);
   $('.fullPageNav__item').removeClass('fullPageNav__item--active');
+  $('.fullPage--active').css({
+    'opacity': '0'
+  });
+  $('.fullPage').eq(index).css({
+    'opacity': '1',
+    'z-index': '50'
+  });
+  setTimeout(() => {
+    $('.fullPage--active').css('z-index', '-1').removeClass('fullPage--active');
+    $('.fullPage').eq(index).addClass('fullPage--active');
+  }, 900);
   e.target.classList.add('fullPageNav__item--active');
 }
 
 const fullPageStart = () => {
-  sections.each((index, section) => {
-    console.log(section);
-    sections.eq(index).css({
-      'top': `${index}00vh`
-    });
+  sections.eq(0).addClass('fullPage--active').css({
+    'z-index': '50',
+    'opacity': '1'
   })
 }
 
