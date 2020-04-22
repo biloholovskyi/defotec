dad50da9b1ef668d1d6e2fd4593e33fe36378b61
amitil <biloholovskyi>
2020-04-22 12:28:17 +0300

    animation change block

--- app/js/fullpageNav.js
+++ app/js/fullpageNav.js
@@ -1,21 +1,49 @@
 const sections = $('.fullPage');
 
 const fullPageNav = (e) => {
-  const index = +e.target.getAttribute('data-number') - 1;
-  console.log(index);
-  $('.fullPageNav__item').removeClass('fullPageNav__item--active');
-  $('.fullPage--active').css({
-    'opacity': '0'
-  });
-  $('.fullPage').eq(index).css({
-    'opacity': '1',
-    'z-index': '50'
-  });
-  setTimeout(() => {
-    $('.fullPage--active').css('z-index', '-1').removeClass('fullPage--active');
-    $('.fullPage').eq(index).addClass('fullPage--active');
-  }, 900);
-  e.target.classList.add('fullPageNav__item--active');
+  const wrapper = $('.fullPageNav');
+  const animBlock = $('.anim-block');
+  const animBlockItem = $('.anim-block__item');
+  if(wrapper.attr('status') !== 'process') {
+    wrapper.attr('status', 'process');
+    // включение анимации
+    animBlock.css({
+      'z-index': '9999'
+    });
+
+    setTimeout(() => {
+      animBlock.css({
+        'opacity': '1'
+      });
+    }, 100);
+
+    setTimeout(() => {
+      animBlockItem.addClass('anim-block__item--show');
+    }, 500);
+
+    // переключение блока
+    const index = +e.target.getAttribute('data-number') - 1;
+    $('.fullPageNav__item').removeClass('fullPageNav__item--active');
+    $('.fullPage--active').css({
+      'opacity': '0'
+    });
+    $('.fullPage').eq(index).css({
+      'opacity': '1',
+      'z-index': '50'
+    });
+    setTimeout(() => {
+      $('.fullPage--active').css('z-index', '-1').removeClass('fullPage--active');
+      $('.fullPage').eq(index).addClass('fullPage--active');
+    }, 900);
+    e.target.classList.add('fullPageNav__item--active');
+
+    // показ блока
+    setTimeout(() => {
+      animBlock.removeAttr('style');
+      animBlockItem.removeClass('anim-block__item--show');
+      wrapper.removeAttr('status');
+    }, 2000);
+  }
 }
 
 const fullPageStart = () => {
--- app/js/index.js
+++ app/js/index.js
@@ -4,7 +4,7 @@ import {fullPageNav, fullPageStart} from "./fullpageNav";
 $(document).ready(function() {
   fullPageStart();
 
-  $('.fullPageNav__item').on('click', (e) => fullPageNav(e))
+  $('.fullPageNav__item, .first__button-down').on('click', (e) => fullPageNav(e));
 });
 
 $(window).resize(function () {
