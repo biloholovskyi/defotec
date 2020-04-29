d61a6d4cf8e60f4990e443b84ffe4746da4d872b
amitil <biloholovskyi>
2020-04-29 15:15:24 +0300

    particle

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,10 @@
 import 'normalize.css';
 import {fullPageNav, fullPageStart, fullPageResize} from "./fullpageNav";
+import './particles';
 
 $(document).ready(function() {
   fullPageStart();
+  particlesJS.load('about__particle', 'particles.json', function() {});
 
   $('.fullPageNav__item, .first__button-down').on('click', (e) => fullPageNav(e));
 });
