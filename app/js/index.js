fbbbd18fdf3b72ba496dc8cd212bbe896a7ed255
amitil <biloholovskyi>
2020-04-30 10:30:31 +0300

    man slider

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css';
 import {fullPageNav, fullPageStart, fullPageResize} from "./fullpageNav";
 import './particles';
+import {manSliderNext, manSliderClick} from "./manSlider";
 
 $(document).ready(function() {
   fullPageStart();
@@ -8,7 +9,11 @@ $(document).ready(function() {
   particlesJS.load('particle-2', 'particles.json', function() {});
 
   $('.fullPageNav__item, .first__button-down').on('click', (e) => fullPageNav(e));
+  $('.home-man__nav .next').on('click', () => manSliderNext('next'));
+  $('.home-man__nav .prev').on('click', manSliderNext);
+  $('.home-man__list .item').on('click', (e) => manSliderClick(e));
 });
+
 
 $(window).resize(function () {
   // if($(window).width() < 992) {
